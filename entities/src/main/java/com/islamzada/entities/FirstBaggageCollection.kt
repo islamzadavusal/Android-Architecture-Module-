9089959d28a5e9ae8820e04aaf7533d8c3752d23
islamzada <vusal.islamzada.01@gmail.com>
2024-01-22 17:23:29 +0400

    ...

---
+++ entities/src/main/java/com/islamzada/entities/FirstBaggageCollection.kt
@@ -0,0 +1,10 @@
+package com.islamzada.entities
+
+data class FirstBaggageCollection(
+    val allowance: Int,
+    val part: Int,
+    val paxType: String,
+    val small: Boolean,
+    val type: String,
+    val unit: String
+)
