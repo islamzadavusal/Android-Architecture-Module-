fe9cb73207e885bd26c85f49710b1985ec33de2d
islamzada <vusal.islamzada.01@gmail.com>
2024-01-24 00:25:37 +0400

    Mapper & Flow

--- features/flights/src/main/java/com/islamzada/flights/searchList/SearchListFragment.kt
+++ features/flights/src/main/java/com/islamzada/flights/searchList/SearchListFragment.kt
@@ -44,7 +44,6 @@ class SearchListFragment : Fragment() {
         }
 
         viewModel.data.observe(viewLifecycleOwner) {
-            //notify adapter
         }
 
         lifecycleScope.launch {
